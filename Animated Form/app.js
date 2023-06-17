f0e8c948f6d70482e7b7816f59e007cdaebab7a7
Manish <manishthakur231690@gmail.com>
2023-06-17 17:43:57 +0530

    Animated-form

--- Animated Form/app.js
+++ Animated Form/app.js
@@ -6,18 +6,21 @@ let btn2 = document.getElementById('btn2');
 
 btn.onclick = function(){
     form1.style.transform = 'translatex(10%)';
-    form1.style.display = 'none';
     text_con.style.transform = 'translatex(-100%)';
-    form2.style.display = 'flex';
-    form2.style.transform = 'translatex(100%)'; 
-    btn.innerHTML = 'Sign Up';
-    btn.setAttribute('id', 'btn2');
+    btn.style.display = 'none';
+    btn2.style.display = 'block';
+    form2.style.right = '0';
+    document.getElementById('heading').innerHTML = 'Welcome Back!';
+    document.getElementById('para').innerHTML = 'To keep connected with us please login </br> with your personal info';
 }
 
 btn2.onclick = function(){
-    form1.style.transform = 'translatex(-10%)';
-    form1.style.display = 'flex';
-    text_con.style.transform = 'translatex(100%)';
-    form2.style.display = 'none';
-    form2.style.transform = 'translatex(-10%)'; 
+    form1.style.transform = 'translatex(0%)';
+    text_con.style.transform = 'translatex(0%)';
+    form2.style.right = '20%';
+    btn.style.display = 'block';
+    btn2.style.display = 'none';
+    
+    document.getElementById('heading').innerHTML = 'Hello, Friends!';
+    document.getElementById('para').innerHTML = 'Enter your personal details and start <br> your journey with us';
 }
